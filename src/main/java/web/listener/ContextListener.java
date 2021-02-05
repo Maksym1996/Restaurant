6c47cad6ca8f514ca841349d24435777ea04b92e
Maks <maxkordonets@gmail.com>
2021-02-05 13:15:20 +0200

    Add orderDao and receiptDao to context

--- src/main/java/web/listener/ContextListener.java
+++ src/main/java/web/listener/ContextListener.java
@@ -10,14 +10,19 @@ import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
 import db.dao.UserDao;
+import db.dao.OrderDao;
 import db.dao.ProductDao;
+import db.dao.ReceiptDao;
+import db.dao.SQLOrderDao;
 import db.dao.SQLProductDao;
+import db.dao.SQLReceiptDao;
 import db.dao.SQLUserDao;
 import exception.ContextInitException;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
 
+	@Override
 	public void contextInitialized(ServletContextEvent event) {
 		initDao(event);
     }
@@ -27,10 +32,14 @@ public class ContextListener implements ServletContextListener {
 				
 		UserDao userDao = new SQLUserDao(dataSource);
 		ProductDao productDao = new SQLProductDao(dataSource);
+		OrderDao orderDao = new SQLOrderDao(dataSource);
+		ReceiptDao receiptDao = new SQLReceiptDao(dataSource);
 		
 		ServletContext servletContext = event.getServletContext();
 		servletContext.setAttribute("userDao", userDao);
 		servletContext.setAttribute("productDao", productDao);
+		servletContext.setAttribute("orderDao", orderDao);
+		servletContext.setAttribute("receiptDao", receiptDao);
 	}
 	
 	private DataSource getDataSource() {
@@ -44,7 +53,4 @@ public class ContextListener implements ServletContextListener {
 		}
 	}
 
-    public void contextDestroyed(ServletContextEvent event) {
-    	
-    }
 }
