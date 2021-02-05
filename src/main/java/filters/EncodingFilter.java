9614493b7798b8239dbf6df1cf5e72659e9ee49e
Maks <maxkordonets@gmail.com>
2021-02-05 19:53:49 +0200

    Solution trouble  with encoding in post request

---
+++ src/main/java/filters/EncodingFilter.java
@@ -0,0 +1,36 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/EncodingFilter")
+public class EncodingFilter implements Filter {
+
+	private String encoding = "utf-8";
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		  request.setCharacterEncoding(encoding);
+	        chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		String encodingParam = fConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+	}
+	
+	 public void destroy() {
+	        // nothing todo
+	    }
+
+}
