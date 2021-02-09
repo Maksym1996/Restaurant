e93c858df989e2041e81b30815fbb967b36a4b8c
Maks <maxkordonets@gmail.com>
2021-02-09 14:47:29 +0200

    Add filter for no access to jsp files

---
+++ src/main/java/filters/NoJSPFilter.java
@@ -0,0 +1,61 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class NoUserFilter
+ */
+@WebFilter("/NoJSPFilter")
+public class NoJSPFilter implements Filter {
+
+	private boolean active = false;
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if (active) {
+			if (!(request instanceof HttpServletRequest && response instanceof HttpServletResponse)) {
+				throw new ServletException("non-HTTP request or response");
+			}
+			HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+			HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+			HttpSession session = httpServletRequest.getSession(true);
+			if (session == null || session.getAttribute("user") == null) {
+				httpServletResponse.sendRedirect("401.html");
+				return;
+			} else {
+				httpServletResponse.sendRedirect("403.html");
+				return;
+			}
+
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		String act = config.getInitParameter("active");
+		if (act != null) {
+			active = (act.equalsIgnoreCase("TRUE"));
+		}
+	}
+
+}
