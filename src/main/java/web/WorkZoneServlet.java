335cd7c5a78ed4358f17280b5448ecc411ab70d5
Maks <maxkordonets@gmail.com>
2021-02-13 16:51:03 +0200

    Add validation to order status

--- src/main/java/web/WorkZoneServlet.java
+++ src/main/java/web/WorkZoneServlet.java
@@ -32,30 +32,10 @@ public class WorkZoneServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String status = request.getParameter("status");
-		String id = request.getParameter("id");
-
-		if (status != null && !"DECLINE".equals(status) && !"CLOSED".equals(status)) {
-
-			List<String> stat = new ArrayList<>();
-			stat.add("NEW");
-			stat.add("APPROVAL");
-			stat.add("COOKING");
-			stat.add("COOKED");
-			stat.add("IN_DELIVERY");
-			stat.add("DELIVERED_AND_PAID");
-			stat.add("CLOSED");
-			status = stat.get(stat.indexOf(status) + 1);
-		}
 
 		HttpSession session = request.getSession(true);
 		String role = (String) session.getAttribute("role");
 		OrderViewDao orderDao = (OrderViewDao) request.getServletContext().getAttribute("orderDao");
-		try {
-			orderDao.updateOrderState(Integer.parseInt(id), status);
-		} catch (Exception e2) {
-			// TODO add logger 09.02
-		}
 
 		List<OrderView> orderViewList = new ArrayList<>();
 		String forwardPage;
@@ -71,16 +51,16 @@ public class WorkZoneServlet extends HttpServlet {
 			break;
 		case "COOK":
 			try {
-				orderViewList = orderDao.getOrdersByStatus("APPROVAL");
+				orderViewList = orderDao.getOrdersByStatus("COOKING");
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 			forwardPage = "Cook.jsp";
 			break;
-		case "DELIVERY":
+		case "COURIER":
 			try {
-				orderViewList = orderDao.getOrdersByStatus("COOKED");
+				orderViewList = orderDao.getOrdersByStatus("IN_DELIVERY");
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -94,26 +74,25 @@ public class WorkZoneServlet extends HttpServlet {
 			response.sendError(401);
 			return;
 		}
-		
+
 		ProductDao productDao = (ProductDao) request.getServletContext().getAttribute("productDao");
 		UserDao userDao = (UserDao) request.getServletContext().getAttribute("userDao");
-		
+
 		Set<Product> productList = new HashSet<>();
 		Set<User> userList = new HashSet<>();
 		Set<OrderView> orders = new HashSet<>();
-	
 		for (OrderView o : orderViewList) {
 			try {
 				orders.add(o);
 				productList.add(productDao.getProduct(o.getProductId()));
 				userList.add(userDao.getUser(o.getUserId()));
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 
-		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(forwardPage);
 		request.setAttribute("orderViewList", orderViewList);
 		request.setAttribute("productList", productList);
@@ -126,8 +105,38 @@ public class WorkZoneServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		String status = request.getParameter("status");
+		String id = request.getParameter("id");
+		
+		OrderViewDao orderDao = (OrderViewDao) request.getServletContext().getAttribute("orderDao");
+
+		if (id != null && status != null && !"REJECTED".equals(status) && !"PERFORMED".equals(status)) {
+
+			List<String> statusList = new ArrayList<>();
+			statusList.add("NEW");
+			statusList.add("COOKING");
+			statusList.add("COOKED");
+			statusList.add("IN_DELIVERY");
+			statusList.add("DELIVERED_AND_PAID");
+			statusList.add("PERFORMED");
+			statusList.add("REJECTED");
+			int indexStatus = statusList.indexOf(status);
+			int orderId = Integer.parseInt(id);
+			try {
+				int currentIndexStatus = statusList.indexOf(orderDao.getStateByOrderId(orderId));
+				if(indexStatus != currentIndexStatus) {
+					response.sendError(400);
+					return;
+				}					
+					status = statusList.get(statusList.indexOf(status) + 1);
+				orderDao.updateOrderState(orderId, status);
+			} catch (Exception e2) {
+				// TODO add logger 09.02
+			}
+		}
+
+		response.sendRedirect("WorkZone");
 	}
 
 }
