a748f0f4ba0857b3a2724685405f810bf5be2804
yzhym <yzhym@DESKTOP-DFF4VLQ>
2021-02-18 21:42:00 +0200

    Init parameters const class

---
+++ src/main/java/consts/Params.java
@@ -0,0 +1,31 @@
+package consts;
+
+public class Params {
+	
+	public static final String PAGE = "page";
+	public static final String CATEGORIES = "categories";
+	public static final String SORT_VALUE = "sortValue";
+	public static final String ASC = "asc";
+	public static final String PRODUCT_ID = "productId";
+	public static final String PRODUCTS_LIST = "productsList";
+	public static final String MAX_PAGES = "maxPages";
+	public static final String CURRENT_PAGE = "currentPage";
+	public static final String CART = "cart";
+	public static final String ROLE = "role";
+	public static final String ORDER_VIEW_LIST = "orderViewList";
+	public static final String ORDERS = "orders";
+	public static final String USER_LIST = "userList";
+	public static final String STATUS = "status";
+	public static final String ID = "id";
+	
+	
+
+	
+	
+	
+	
+	private Params() {
+	    throw new IllegalStateException("Utility class");
+	  }
+
+}
