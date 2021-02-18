933c8947e1723d211ef115894e87f9ac40c4f760
yzhym <yzhym@DESKTOP-DFF4VLQ>
2021-02-18 21:46:57 +0200

    Init Dao context conts

---
+++ src/main/java/consts/Dao.java
@@ -0,0 +1,14 @@
+package consts;
+
+public class Dao {
+	
+	public static final String PRODUCT = "productDao";
+	public static final String USER = "userDao";
+	public static final String ORDER_VIEW = "orderViewDao";
+	
+	
+	private Dao() {
+	    throw new IllegalStateException("Utility class");
+	  }
+
+}
