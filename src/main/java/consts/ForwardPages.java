1e2699e9df43dfc9a78cb529c20ac8bcfc81349f
yzhym <yzhym@DESKTOP-DFF4VLQ>
2021-02-18 21:45:05 +0200

    Init forward and redirect name page const

---
+++ src/main/java/consts/ForwardPages.java
@@ -0,0 +1,13 @@
+package consts;
+
+public class ForwardPages {
+	
+	public static final String PIZZA_PREFERITA = "Pizza Preferita.jsp";
+	public static final String WORK_ZONE = "WorkZone";
+	
+	private ForwardPages() {
+	    throw new IllegalStateException("Utility class");
+	  }
+	
+
+}
