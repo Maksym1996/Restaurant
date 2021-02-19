62830c2a2341c07c2e2a098d954676fa24401f64
yzhym <yzhym@DESKTOP-DFF4VLQ>
2021-02-19 03:10:21 +0200

    Checked all headers

--- src/main/java/web/LanguageServlet.java
+++ src/main/java/web/LanguageServlet.java
@@ -14,23 +14,20 @@ public class LanguageServlet extends HttpServlet {
 	private static final long serialVersionUID = 2689567392392305656L;
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String lang = request.getParameter("lang");
-        String path = request.getHeader("Referer");
-  
-        
-        HttpSession session = request.getSession(true);
-        session.setAttribute("lang", lang);
-        response.sendRedirect(path);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String lang = request.getParameter("lang");
+		String path = request.getHeader("referer");
 
+		HttpSession session = request.getSession(true);
+		session.setAttribute("lang", lang);
+		response.sendRedirect(path);
+	}
 
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    	doGet(request, response);
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
