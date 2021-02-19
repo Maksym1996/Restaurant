fc193a3c3ff2a7c845f6bc04602a296920ce9272
yzhym <yzhym@DESKTOP-DFF4VLQ>
2021-02-19 03:08:08 +0200

    Refartor: remove valid to Validator.java

--- src/main/java/web/UpdateProductServlet.java
+++ src/main/java/web/UpdateProductServlet.java
@@ -1,9 +1,7 @@
 package web;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,10 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import consts.Dao;
+import consts.Page;
+import consts.Param;
 import db.dao.ProductDao;
 import db.entity.Product;
+import exception.DBException;
 import util.Category;
 import util.Util;
+import util.Validator;
 
 /**
  * Servlet implementation class UpdateProductServlet
@@ -27,39 +30,31 @@ public class UpdateProductServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String patternId = "[1-9]{1}[0-9]*";
 
-		String productId = request.getParameter("id");
+		String productId = request.getParameter(Param.ID);
 
-		if (productId == null || productId.isEmpty()) {
-			response.sendError(400);
-			return;
-		}
-
-		if (!Pattern.matches(patternId, productId)) {
+		if (!Validator.intValidator(productId)) {
 			response.sendError(415);
 			return;
 		}
-		ProductDao productDao = (ProductDao) request.getServletContext().getAttribute("productDao");
+
+		ProductDao productDao = (ProductDao) request.getServletContext().getAttribute(Dao.PRODUCT);
 		Product testProduct;
 		try {
 			testProduct = productDao.getProductById(Integer.parseInt(productId));
-		} catch (NumberFormatException e) {
+		} catch (DBException e) {
 			// TODO Auto-generated catch block
 			System.err.println(e);
-			throw new IOException();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.err.println(e);
-			throw new IOException();
+			response.sendError(500);
+			return;
 		}
 		if (testProduct == null) {
 			response.sendError(416);
 			return;
 		}
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("UpdateProduct.jsp");
-		request.setAttribute("product", testProduct);
+		RequestDispatcher dispatcher = request.getRequestDispatcher(Page.UPDATE_PRODUCT_JSP);
+		request.setAttribute(Param.PRODUCT, testProduct);
 		dispatcher.forward(request, response);
 
 	}
@@ -67,87 +62,52 @@ public class UpdateProductServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String pricePattern = "[1-9]{1}[0-9]*";
 
-		String name = request.getParameter("name");
-		String price = request.getParameter("price");
-		String description = request.getParameter("description");
-		String imageLink = request.getParameter("imageLink");
-		String category = request.getParameter("category");
-		String id = request.getParameter("id");
+		String name = request.getParameter(Param.NAME);
+		String price = request.getParameter(Param.PRICE);
+		String description = request.getParameter(Param.DESCRIPTION);
+		String imageLink = request.getParameter(Param.IMAGE_LINK);
+		String category = request.getParameter(Param.CATEGORY);
+		String id = request.getParameter(Param.ID);
 
-		Map<String, String> errors = new HashMap<>();
+		Map<String, String> errors = Validator.productValidator(name, price, description, imageLink, category);
 
-		if (name == null || name.isEmpty()) {
-			errors.put("name", "Enter product name");
-		}
-
-		if (price == null || price.isEmpty()) {
-			errors.put("price", "Enter product price");
-		} else if (!Pattern.matches(pricePattern, price)) {
-			errors.put("pricePattern", "The price must be an integer and not start from zero");
-		}
-
-		if (description == null || description.isEmpty()) {
-			errors.put("description", "Enter product description");
-		}
-
-		if (imageLink == null || imageLink.isEmpty()) {
-			errors.put("imageLink", "Enter product imageLink");
-		}
-
-		if (category == null || category.isEmpty()) {
-			errors.put("category", "Enter product category");
-		}
-		Category categoryObject = Category.byTitle(category);
-		if (categoryObject == null) {
-			errors.put("category", "The '" + category + "' is not a valid category");
-		}
-
-		int productId = 0;
-		ProductDao productDao = (ProductDao) request.getServletContext().getAttribute("productDao");
-		Product product;
+		int productId = Integer.parseInt(id);
+		ProductDao productDao = (ProductDao) request.getServletContext().getAttribute(Dao.PRODUCT);
 		try {
-			product = productDao.getProductById(productId);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			System.err.println("Get Product: " + e1);
+			Product testProductById = productDao.getProductById(productId);
+			Product testProductByName = productDao.getProductByName(name);
+
+			if (testProductById == null || !Validator.intValidator(id)) {
+				response.sendError(416);
+				return;
+			} else if (testProductByName != null) {
+				errors.put(Param.NAME, "The name '" + name + "' is taken");
+			}
+
+		} catch (DBException e) {
 			response.sendError(500);
 			return;
-		}
-
-		if (id == null || id.isEmpty()) {
-			errors.put("id", "Product id was not found");
-		} else if (!Pattern.matches(pricePattern, id)) {
-			errors.put("idPattern", "The id must be an integer and not start from zero");
-		} else if (product == null) {
-			productId = Integer.parseInt(id);
-			errors.put("idNone", "The product does not exist for the given id = " + productId + " !");
 		}
 
 		if (!errors.isEmpty()) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("AddProduct.jsp");
-			request.setAttribute("errors", errors);
+			RequestDispatcher dispatcher = request.getRequestDispatcher(Page.ADD_PRODUCT_JSP);
+			request.setAttribute(Param.ERRORS, errors);
 			dispatcher.forward(request, response);
 			return;
 		}
-		productId = Integer.parseInt(id);
+
 		try {
 			productDao.updateProduct(Util.createProduct(name, Integer.parseInt(price), description, imageLink,
-					categoryObject, productId));
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			System.err.println("Update Product Number Format" + e);
-			response.sendError(500);
-			return;
-		} catch (Exception e) {
+					Category.valueOf(category), productId));
+		} catch (DBException e) {
 			// TODO Auto-generated catch block
 			System.err.println("Update product exception: " + e);
 			response.sendError(500);
 			return;
 		}
 
-		response.sendRedirect("Pizza Preferita");
+		response.sendRedirect(Page.PIZZA_PREFERITA);
 	}
 
 }
