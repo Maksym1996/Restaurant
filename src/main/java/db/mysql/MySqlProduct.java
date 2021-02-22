39cb81ee0429fcf137be9bc25b1bc46acfce844e
Maksym <max@max>
2021-02-22 13:22:15 +0200

    Change variables name

--- src/main/java/db/mysql/MySqlProduct.java
+++ src/main/java/db/mysql/MySqlProduct.java
@@ -16,8 +16,9 @@ import db.dao.ProductDao;
 import db.entity.Product;
 import exception.DBException;
 import util.Category;
+
 /**
- *The class implementing ProductDao for DBMS MySQL
+ * The class implementing ProductDao for DBMS MySQL
  */
 public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 	private static final String INSERT_PRODUCT = "INSERT INTO product VALUES (DEFAULT, ?, ?, ?, ?, ?)";
@@ -27,7 +28,7 @@ public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 			+ "price=?, description=?, image_link=?, category=?  WHERE id = ?";
 	private static final String DELETE_PRODUCT_BY_ID = "DELETE FROM product WHERE id = ?";
 
-	private DataSource dataSource;
+	private final DataSource dataSource;
 
 	public MySqlProduct(DataSource dataSource) {
 		this.dataSource = dataSource;
@@ -37,214 +38,248 @@ public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 	public List<Product> getProductByCategoriesOnPage(String[] categories, String sortValue, String desc, int skip,
 			int limit) throws DBException {
 		log.info(Comment.BEGIN);
-		List<Product> productByCategories = new ArrayList<>();
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		List<Product> productsByCategories = new ArrayList<>();
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		log.debug("categories = " + categories.length);
 		log.debug("sortValue = " + sortValue);
 		log.debug("desc = " + desc);
 		log.debug("skip = " + skip);
 		log.debug("limit = " + limit);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(protectSqlInjection(sortValue, categories, desc));
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(protectSqlInjection(sortValue, categories, desc));
 
 			int k = 1;
 			for (String category : categories) {
-				prep.setString(k++, category);
+				preparedStatement.setString(k++, category);
 			}
-			prep.setInt(k++, limit);
-			prep.setInt(k++, skip);
+			preparedStatement.setInt(k++, limit);
+			preparedStatement.setInt(k, skip);
 
-			rs = prep.executeQuery();
+			resultSet = preparedStatement.executeQuery();
 
-			while (rs.next()) {
-				productByCategories.add(extractionProduct(rs));
+			while (resultSet.next()) {
+				productsByCategories.add(extractionProduct(resultSet));
 			}
 
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
-		log.debug(Comment.RETURN + productByCategories.size());
-		return productByCategories;
+		log.debug(Comment.RETURN + productsByCategories.size());
+		return productsByCategories;
 	}
 
 	@Override
-	public long getProductCount(String[] categories) throws DBException {
+	public long getProductsCount(String[] categories) throws DBException {
 		log.info(Comment.BEGIN);
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 
-		int res = 0;
-		log.debug("categories = " + categories);
+		int countProducts = 0;
+		log.debug("categories " + categories);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(countQuery(categories));
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(countQuery(categories));
 			int k = 1;
 			for (String category : categories) {
-				prep.setString(k++, category);
+				preparedStatement.setString(k++, category);
 			}
-			rs = prep.executeQuery();
+			resultSet = preparedStatement.executeQuery();
 
-			if (rs.next()) {
-				res = rs.getInt(1);
+			if (resultSet.next()) {
+				countProducts = resultSet.getInt(1);
 			}
 
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
-		log.debug(Comment.RETURN + res);
-		return res;
+		log.debug(Comment.RETURN + countProducts);
+		return countProducts;
 	}
 
 	@Override
-	public int insertProduct(Product model) throws DBException {
+	public int insertProduct(Product product) throws DBException {
 		log.info(Comment.BEGIN);
 		int productId = 0;
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
-		log.debug("product = " + model.toString());
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		log.debug("product = " + product.toString());
 		try {
-			con = dataSource.getConnection();
-			con.setAutoCommit(false);
-			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-			prep = con.prepareStatement(INSERT_PRODUCT, Statement.RETURN_GENERATED_KEYS);
-			int k = 1;
-			prep.setString(k++, model.getName());
-			prep.setInt(k++, model.getPrice());
-			prep.setString(k++, model.getDescription());
-			prep.setString(k++, model.getImageLink());
-			prep.setString(k++, model.getCategory().toString());
+			connect = dataSource.getConnection();
+			connect.setAutoCommit(false);
+			connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			preparedStatement = connect.prepareStatement(INSERT_PRODUCT, Statement.RETURN_GENERATED_KEYS);
 
-			if (prep.executeUpdate() > 0) {
-				rs = prep.getGeneratedKeys();
-				if (rs.next()) {
-					productId = rs.getInt(1);
-					model.setId(productId);
+			int k = 1;
+			preparedStatement.setString(k++, product.getName());
+			preparedStatement.setInt(k++, product.getPrice());
+			preparedStatement.setString(k++, product.getDescription());
+			preparedStatement.setString(k++, product.getImageLink());
+			preparedStatement.setString(k, product.getCategory().toString());
+
+			if (preparedStatement.executeUpdate() > 0) {
+				resultSet = preparedStatement.getGeneratedKeys();
+				if (resultSet.next()) {
+					productId = resultSet.getInt(1);
+					product.setId(productId);
 				}
 			}
-			con.commit();
+			connect.commit();
 			log.debug(Comment.COMMIT);
 		} catch (SQLException e) {
 			log.error(Comment.COMMIT + e.getMessage());
-			rollback(con);
+			rollback(connect);
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
 		log.debug(Comment.RETURN + productId);
 		return productId;
 	}
 
 	@Override
-	public Product getProductById(int id) throws DBException {
+	public Product getProductById(int productId) throws DBException {
 		log.info(Comment.BEGIN);
-		Product model = null;
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
-		log.debug("id = " + id);
+		Product product = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		log.debug("id = " + productId);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(SELECT_PRODUCT_BY_ID);
-			prep.setInt(1, id);
-			rs = prep.executeQuery();
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(SELECT_PRODUCT_BY_ID);
+			preparedStatement.setInt(1, productId);
+			resultSet = preparedStatement.executeQuery();
 
-			if (rs.next()) {
-				model = extractionProduct(rs);
+			if (resultSet.next()) {
+				product = extractionProduct(resultSet);
 			}
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
-		log.debug(Comment.RETURN + model);
-		return model;
+		log.debug(Comment.RETURN + product);
+		return product;
 	}
 
 	@Override
 	public Product getProductByName(String name) throws DBException {
 		log.info(Comment.BEGIN);
-		Product model = null;
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Product product = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		log.debug("name " + name);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(SELECT_PRODUCT_BY_NAME);
-			prep.setString(1, name);
-			rs = prep.executeQuery();
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(SELECT_PRODUCT_BY_NAME);
+			preparedStatement.setString(1, name);
+			resultSet = preparedStatement.executeQuery();
 
-			if (rs.next()) {
-				model = extractionProduct(rs);
+			if (resultSet.next()) {
+				product = extractionProduct(resultSet);
 			}
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
-		log.debug(Comment.RETURN + model);
-		return model;
+		log.debug(Comment.RETURN + product);
+		return product;
 	}
 
 	@Override
-	public boolean updateProduct(Product model) throws DBException {
+	public boolean updateProduct(Product product) throws DBException {
 		log.info(Comment.BEGIN);
 		boolean result = false;
-		Connection con = null;
-		PreparedStatement prep = null;
-		log.debug("Product = " + model.toString());
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		log.debug("Product = " + product.toString());
 		try {
-			con = dataSource.getConnection();
-			con.setAutoCommit(false);
-			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-			prep = con.prepareStatement(UPDATE_PRODUCT_BY_ID);
+			connect = dataSource.getConnection();
+			connect.setAutoCommit(false);
+			connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			preparedStatement = connect.prepareStatement(UPDATE_PRODUCT_BY_ID);
 			int k = 1;
-			prep.setString(k++, model.getName());
-			prep.setInt(k++, model.getPrice());
-			prep.setString(k++, model.getDescription());
-			prep.setString(k++, model.getImageLink());
-			prep.setString(k++, model.getCategory().toString());
-			prep.setInt(k++, model.getId());
+			preparedStatement.setString(k++, product.getName());
+			preparedStatement.setInt(k++, product.getPrice());
+			preparedStatement.setString(k++, product.getDescription());
+			preparedStatement.setString(k++, product.getImageLink());
+			preparedStatement.setString(k++, product.getCategory().toString());
+			preparedStatement.setInt(k, product.getId());
 
-			if (prep.executeUpdate() > 0) {
+			if (preparedStatement.executeUpdate() > 0) {
 				result = true;
 			}
-			con.commit();
+			connect.commit();
 			log.debug(Comment.COMMIT);
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
-			rollback(con);
+			rollback(connect);
 			throw new DBException(e);
 		} finally {
-			close(con, prep);
+			close(connect, preparedStatement);
 		}
 		log.debug(Comment.RETURN + result);
 		return result;
 
 	}
 
-	private Product extractionProduct(ResultSet rs) throws SQLException {
+	@Override
+	public boolean deleteProductById(int productId) throws DBException {
+		log.info(Comment.BEGIN);
+		log.debug("id = " + productId);
+		boolean result = false;
+
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connect = dataSource.getConnection();
+			connect.setAutoCommit(false);
+			connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			preparedStatement = connect.prepareStatement(DELETE_PRODUCT_BY_ID);
+			preparedStatement.setInt(1, productId);
+
+			if (preparedStatement.executeUpdate() > 0) {
+				result = true;
+			}
+			connect.commit();
+			log.debug(Comment.COMMIT);
+		} catch (SQLException e) {
+			log.error(Comment.SQL_EXCEPTION + e.getMessage());
+			rollback(connect);
+			throw new DBException(e);
+		} finally {
+			close(connect, preparedStatement, resultSet);
+		}
+		log.debug(Comment.RETURN + result);
+		return result;
+	}
+
+	private Product extractionProduct(ResultSet resultSet) throws SQLException {
 		Product product = new Product();
 		int k = 1;
-		product.setId(rs.getInt(k++));
-		product.setName(rs.getString(k++));
-		product.setPrice(rs.getInt(k++));
-		product.setDescription(rs.getString(k++));
-		product.setImageLink(rs.getString(k++));
-		product.setCategory(Category.byTitle(rs.getString(k)));
+		product.setId(resultSet.getInt(k++));
+		product.setName(resultSet.getString(k++));
+		product.setPrice(resultSet.getInt(k++));
+		product.setDescription(resultSet.getString(k++));
+		product.setImageLink(resultSet.getString(k++));
+		product.setCategory(Category.byTitle(resultSet.getString(k)));
 
 		log.debug(Comment.EXTRACTION + product.toString());
 		return product;
@@ -260,8 +295,8 @@ public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 		return result.toString();
 
 	}
-	
-	private String protectSqlInjection(String value, String[] categories, String desc) {
+
+	private String protectSqlInjection(String sortBy, String[] categories, String desc) {
 		StringBuilder result = new StringBuilder();
 		result.append("SELECT * FROM product ");
 		if (categories != null && categories.length != 0) {
@@ -269,18 +304,18 @@ public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 			result.append("WHERE category IN (").append(inSql).append(") ");
 		}
 
-		switch (value) {
+		switch (sortBy) {
 		case "name":
-			result.append("ORDER BY name ").toString();
+			result.append("ORDER BY name ");
 			break;
 		case "price":
-			result.append("ORDER BY price ").toString();
+			result.append("ORDER BY price ");
 			break;
 		case "category":
-			result.append("ORDER BY category ").toString();
+			result.append("ORDER BY category ");
 			break;
 		default:
-			result.append("ORDER BY id ").toString();
+			result.append("ORDER BY id ");
 			break;
 		}
 
@@ -291,40 +326,6 @@ public class MySqlProduct extends AbstractMySqlDao implements ProductDao {
 		}
 
 		return result.append("LIMIT ? OFFSET ?").toString();
-
-	}
-
-	@Override
-	public boolean deleteProductById(int id) throws DBException {
-		log.info(Comment.BEGIN);
-		log.debug("id = " + id);
-		boolean result = false;
-
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
-
-		try {
-			con = dataSource.getConnection();
-			con.setAutoCommit(false);
-			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-			prep = con.prepareStatement(DELETE_PRODUCT_BY_ID);
-			prep.setInt(1, id);
-
-			if (prep.executeUpdate() > 0) {
-				result = true;
-			}
-			con.commit();
-			log.debug(Comment.COMMIT);
-		} catch (SQLException e) {
-			log.error(Comment.SQL_EXCEPTION + e.getMessage());
-			rollback(con);
-			throw new DBException(e);
-		} finally {
-			close(con, prep, rs);
-		}
-		log.debug(Comment.RETURN + result);
-		return result;
 
 	}
 
