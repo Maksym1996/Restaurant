d67059104ee4999ed4bf31e1468744652ed7c045
Maksym <max@max>
2021-02-22 15:17:31 +0200

    Change data

--- src/main/resources/insert__to_db.sql
+++ src/main/resources/insert__to_db.sql
@@ -8,21 +8,15 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `Restaurant`;
-INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (1, 'kordonets1996@ukr.net', 'Максим', 'Кордонец', '36d2e385ff8453a66347bf048f11668c', '0969055386', 'MANAGER', 'true');
-INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (2, 'Povar@ukr.net', 'Повар', 'Куховаров', '6bb19089370f5bb5478f7ec1b337f255', '0969055385', 'COOK', 'true');
-INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (3, 'Courier@ukr.net', 'Курьер', 'Доставщиков', '6bb19089370f5bb5478f7ec1b337f255', '0969055384', 'COURIER', 'true');
-INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (4, 'kordonetsmax@gmail.com', 'Клиент', 'Посетитович', '6bb19089370f5bb5478f7ec1b337f255', '0969055383', 'CLIENT', 'true');
-INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (5, 'maxkorodnets@gmail.com', 'Админ', 'Распорядитович', '6bb19089370f5bb5478f7ec1b337f255', '0969055382', 'ADMIN', 'true');
-
-COMMIT;
-
-
+INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (1, 'manager@ukr.net', 'Менеджер', 'Управляевич', '36d2e385ff8453a66347bf048f11668c', '0123456781', 'MANAGER', 'true');
+INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (2, 'povar@ukr.net', 'Повар', 'Куховаров', '36d2e385ff8453a66347bf048f11668c', '0123456782', 'COOK', 'true');
+INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (3, 'courier@ukr.net', 'Курьер', 'Доставщиков', '36d2e385ff8453a66347bf048f11668c', '0123456783', 'COURIER', 'true');
+INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (4, 'client@ukr.net', 'Клиент', 'Посетитович', '36d2e385ff8453a66347bf048f11668c', '0123456784', 'CLIENT', 'true');
+INSERT INTO `Restaurant`.`user` (`id`, `email`, `first_name`, `last_name`, `password`, `phone_number`, `role`, `registered`) VALUES (5, 'admin@ukr.net', 'Админ', 'Распорядитович', '36d2e385ff8453a66347bf048f11668c', '0123456780', 'ADMIN', 'true');
 
 -- -----------------------------------------------------
 -- Data for table `Restaurant`.`product`
 -- -----------------------------------------------------
-START TRANSACTION;
-USE `Restaurant`;
 INSERT INTO `Restaurant`.`product` (`id`, `name`, `price`, `description`, `image_link`, `category`) VALUES (1, 'Паперони', 320, 'Сыр, мясо и паперони', 'https://raw.githubusercontent.com/Maksym1996/Image_Restaurant/main/Paperonni.jpg', 'Pizza');
 INSERT INTO `Restaurant`.`product` (`id`, `name`, `price`, `description`, `image_link`, `category`) VALUES (2, 'Маргатира', 250, 'Сыр, помидоры', 'https://raw.githubusercontent.com/Maksym1996/Image_Restaurant/main/Margarita.jpg', 'Pizza');
 
