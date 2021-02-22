97cc87cb6f519bc68cf4338429c59d2cba938676
Maksym <max@max>
2021-02-22 13:24:33 +0200

    Change variables name

--- src/main/java/db/mysql/MySqlOrderView.java
+++ src/main/java/db/mysql/MySqlOrderView.java
@@ -40,23 +40,23 @@ public class MySqlOrderView extends AbstractMySqlDao implements OrderViewDao {
 	public String getStatusByOrderId(int orderId) throws DBException {
 		log.info(Comment.BEGIN);
 		String state = null;
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		log.debug("orderId = " + orderId);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(SELECT_STATUS_BY_ORDER_ID);
-			prep.setInt(1, orderId);
-			rs = prep.executeQuery();
-			if (rs.next()) {
-				state = rs.getString(1);
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(SELECT_STATUS_BY_ORDER_ID);
+			preparedStatement.setInt(1, orderId);
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				state = resultSet.getString(1);
 			}
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
 		log.debug(Comment.RETURN + state);
 		return state;
@@ -66,22 +66,22 @@ public class MySqlOrderView extends AbstractMySqlDao implements OrderViewDao {
 	public List<OrderView> getAllOrderViews() throws DBException {
 		log.info(Comment.BEGIN);
 		List<OrderView> allOrders = new ArrayList<>();
-		Connection con = null;
-		Statement stat = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
 		try {
-			con = dataSource.getConnection();
-			stat = con.createStatement();
-			rs = stat.executeQuery(SELECT_ORDER_VIEWS_FOR_MANAGER);
-			while (rs.next()) {
-				allOrders.add(extractionOrder(rs));
+			connect = dataSource.getConnection();
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery(SELECT_ORDER_VIEWS_FOR_MANAGER);
+			while (resultSet.next()) {
+				allOrders.add(extractionOrder(resultSet));
 			}
 
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, stat, rs);
+			close(connect, statement, resultSet);
 		}
 		log.debug(Comment.RETURN + allOrders.size());
 		return allOrders;
@@ -91,24 +91,24 @@ public class MySqlOrderView extends AbstractMySqlDao implements OrderViewDao {
 	public List<OrderView> getOrderViewsByUserId(int userId) throws DBException {
 		log.info(Comment.BEGIN);
 		List<OrderView> allOrders = new ArrayList<>();
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		log.debug("userId = " + userId);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(SELECT_ORDER_VIEWS_BY_USER_ID);
-			prep.setInt(1, userId);
-			rs = prep.executeQuery();
-			while (rs.next()) {
-				allOrders.add(extractionOrder(rs));
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(SELECT_ORDER_VIEWS_BY_USER_ID);
+			preparedStatement.setInt(1, userId);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				allOrders.add(extractionOrder(resultSet));
 			}
 
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
 		log.debug(Comment.RETURN + allOrders.size());
 		return allOrders;
@@ -118,133 +118,131 @@ public class MySqlOrderView extends AbstractMySqlDao implements OrderViewDao {
 	public List<OrderView> getOrdersByStatus(String status) throws DBException {
 		log.info(Comment.BEGIN);
 		List<OrderView> allOrders = new ArrayList<>();
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		log.debug("status = " + status);
 		try {
-			con = dataSource.getConnection();
-			prep = con.prepareStatement(SELECT_ORDER_VIEWS_BY_STATUS);
-			prep.setString(1, status);
-			rs = prep.executeQuery();
-			while (rs.next()) {
-				allOrders.add(extractionOrder(rs));
+			connect = dataSource.getConnection();
+			preparedStatement = connect.prepareStatement(SELECT_ORDER_VIEWS_BY_STATUS);
+			preparedStatement.setString(1, status);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				allOrders.add(extractionOrder(resultSet));
 			}
 
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
 		log.debug(Comment.RETURN + allOrders.size());
 		return allOrders;
 	}
 
 	@Override
-	public int insertOrder(OrderView model, List<Product> products, Map<Integer, Integer> count) throws DBException {
+	public int insertOrder(OrderView orderView, List<Product> products, Map<Integer, Integer> count) throws DBException {
 		log.info(Comment.BEGIN);
-		Connection con = null;
-		PreparedStatement prep = null;
-		ResultSet rs = null;
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
 		int orderId = 0;
-		log.debug("model = " + model.toString());
-		log.debug("products = " + products.toArray());
-		log.debug("count = " + count.size());
+		log.debug("model = " + orderView);
+		log.debug("products = " + products);
+		log.debug("count = " + count);
 		try {
 
-			con = dataSource.getConnection();
-			con.setAutoCommit(false);
-			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-			prep = con.prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);
+			connect = dataSource.getConnection();
+			connect.setAutoCommit(false);
+			connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			preparedStatement = connect.prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);
 			int k = 1;
-			prep.setString(k++, model.getStatus().name());
-			prep.setString(k++, model.getAddress());
-			prep.setInt(k++, model.getUserId());
-			prep.setString(k++, model.getSum());
+			preparedStatement.setString(k++, orderView.getStatus().name());
+			preparedStatement.setString(k++, orderView.getAddress());
+			preparedStatement.setInt(k++, orderView.getUserId());
+			preparedStatement.setString(k, orderView.getSum());
 
-			if (prep.executeUpdate() > 0) {
-				rs = prep.getGeneratedKeys();
-				if (rs.next()) {
-					orderId = rs.getInt(1);
-					model.setId(orderId);
+			if (preparedStatement.executeUpdate() > 0) {
+				resultSet = preparedStatement.getGeneratedKeys();
+				if (resultSet.next()) {
+					orderId = resultSet.getInt(1);
+					orderView.setId(orderId);
 				}
 			}
-			for (Product p : products) {
-				try (PreparedStatement setProdPrepSt = con.prepareStatement(INSERT_PRODUCT_FOR_ORDER)) {
+			for (Product product : products) {
+				try (PreparedStatement setProdPrepSt = connect.prepareStatement(INSERT_PRODUCT_FOR_ORDER)) {
 					k = 1;
 					setProdPrepSt.setInt(k++, orderId);
-					setProdPrepSt.setInt(k++, p.getId());
-					setProdPrepSt.setInt(k++, count.get(p.getId()));
-					setProdPrepSt.setInt(k++, p.getPrice());
+					setProdPrepSt.setInt(k++, product.getId());
+					setProdPrepSt.setInt(k++, count.get(product.getId()));
+					setProdPrepSt.setInt(k, product.getPrice());
 					setProdPrepSt.executeUpdate();
 				}
 
 			}
-			con.commit();
+			connect.commit();
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
-			rollback(con);
+			rollback(connect);
 			throw new DBException(e);
 		} finally {
-			close(con, prep, rs);
+			close(connect, preparedStatement, resultSet);
 		}
 		log.debug(Comment.RETURN + orderId);
 		return orderId;
 	}
 
 	@Override
-	public boolean updateStatusById(int id, String status) throws DBException {
+	public boolean updateStatusById(int orderId, String status) throws DBException {
 		log.info(Comment.BEGIN);
 		String setState = "UPDATE orders SET state=? WHERE id = ?";
 		boolean result = false;
-		Connection con = null;
-		PreparedStatement prep = null;
-		log.debug("id = " + id);
+		Connection connect = null;
+		PreparedStatement preparedStatement = null;
+		log.debug("id = " + orderId);
 		log.debug("status = " + status);
 		try {
-			con = dataSource.getConnection();
-			con.setAutoCommit(false);
-			con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+			connect = dataSource.getConnection();
+			connect.setAutoCommit(false);
+			connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 			if (Status.REJECTED.name().equals(status) || Status.PERFORMED.name().equals(status)) {
 				setState = "UPDATE orders SET state=?, closing_date = current_timestamp() WHERE id = ? ";
 			}
-			prep = con.prepareStatement(setState);
-			prep.setString(1, status);
-			prep.setInt(2, id);
-			if (prep.executeUpdate() > 0) {
+			preparedStatement = connect.prepareStatement(setState);
+			preparedStatement.setString(1, status);
+			preparedStatement.setInt(2, orderId);
+			if (preparedStatement.executeUpdate() > 0) {
 				result = true;
 			}
-			con.commit();
+			connect.commit();
 			log.debug(Comment.COMMIT);
 		} catch (SQLException e) {
 			log.error(Comment.SQL_EXCEPTION + e.getMessage());
-			rollback(con);
+			rollback(connect);
 			throw new DBException(e);
 		} finally {
-			close(con, prep);
+			close(connect, preparedStatement);
 		}
 		log.debug(Comment.RETURN + result);
 		return result;
 
 	}
 
-	private OrderView extractionOrder(ResultSet rs) throws SQLException {
-		OrderView order = new OrderView();
+	private OrderView extractionOrder(ResultSet resultSet) throws SQLException {
+		OrderView orderView = new OrderView();
 		int k = 1;
-		order.setId(rs.getInt(k++));
-		order.setOrderDate(rs.getString(k++));
-		order.setClosingDate(rs.getString(k++));
-		order.setStatus(Status.valueOf(rs.getString(k++)));
-		order.setAddress(rs.getString(k++));
-		order.setUserId(rs.getInt(k++));
-		order.setSum(rs.getString(k++));
-		order.setOrderId(rs.getInt(k++));
-		order.setProductId(rs.getInt(k++));
-		order.setCount(rs.getInt(k++));
-		order.setPrice(rs.getInt(k));
-		log.debug(Comment.EXTRACTION + order.toString());
-		return order;
+		orderView.setId(resultSet.getInt(k++));
+		orderView.setStatus(Status.valueOf(resultSet.getString(k++)));
+		orderView.setAddress(resultSet.getString(k++));
+		orderView.setUserId(resultSet.getInt(k++));
+		orderView.setSum(resultSet.getString(k++));
+		orderView.setOrderId(resultSet.getInt(k++));
+		orderView.setProductId(resultSet.getInt(k++));
+		orderView.setCount(resultSet.getInt(k++));
+		orderView.setPrice(resultSet.getInt(k));
+		log.debug(Comment.EXTRACTION + orderView.toString());
+		return orderView;
 	}
 
 }
