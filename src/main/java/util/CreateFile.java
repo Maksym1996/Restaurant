1a19d46122824c1ba622ab2b18b8a11dc9bef00d
Maksym <max@max>
2021-02-26 02:19:23 +0200

    Impl cyrillic in pdf report

--- src/main/java/util/CreateFile.java
+++ src/main/java/util/CreateFile.java
@@ -1,7 +1,5 @@
 package util;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.List;
 
@@ -9,13 +7,13 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import consts.Path;
 import db.entity.Order;
 import db.entity.OrderContent;
 import db.entity.Receipt;
@@ -27,21 +25,29 @@ public class CreateFile {
 		// nothing
 	}
 
+	private static final Font TITLE_FONT = new Font(Font.FontFamily.TIMES_ROMAN, 24, Font.BOLD);
+	private static final Font TABLE_FORT = FontFactory.getFont("FreeSans.ttf", "Cp1251", true);
+
 	private static final String SPACE = " ";
-	private static final String SELLER = "Maksym Ltd. ";
-	
-
-	private static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 24, Font.BOLD);
-	private static final String ORDER_NUMBER = "Order number # ";
+	private static final String SELLER_LABEL = "Maksym Ltd. ";
+	private static final String ORDER_NUMBER = "Заказ номер ";
 	private static final int COLUMN_COUNT = 3;
+	private static final String LABEL = "Pizza Preferita";
+	private static final String ORDER_SUM = "Сумма заказа: ";
+	private static final String GRN = "грн";
+	private static final String CUSTOMER = "Покупатель: ";
+	private static final String SELLER = "Продавец: ";
+	private static final String NAME = "Название";
+	private static final String QUANTITY = "Количество(шт)";
+	private static final String PRICE_GRN = "Цена(грн)";
+	private static final String OPEN_DATE = "Заказ получен";
+	private static final String CLOSE_DATE = "Заказ закрыт";
 
-	public static File createPdfFile(Receipt receipt) throws DBException {
+	public static void writePdfFile(Receipt receipt, OutputStream outStream) throws DBException {
 		Order order = receipt.getOrder();
 		List<OrderContent> orderContent = receipt.getOrderContent();
-		File reportFile = new File(Path.REPORT_PDF);
 		try {
 			Document document = new Document();
-			OutputStream outStream = new FileOutputStream(reportFile);
 			PdfWriter.getInstance(document, outStream);
 			document.open();
 			addContent(document, order, orderContent);
@@ -49,73 +55,76 @@ public class CreateFile {
 		} catch (Exception e) {
 			throw new DBException(e);
 		}
-		return reportFile;
 	}
 
 	private static void addContent(Document document, Order order, List<OrderContent> listOrderContent)
 			throws DocumentException {
-		Paragraph paragraphTitle = new Paragraph("Pizza Preferita");
-		paragraphTitle.setAlignment(Element.ALIGN_RIGHT);
-		paragraphTitle.setFont(titleFont);
-		addEmptyLine(paragraphTitle, 3);
+		Paragraph paragraphTitle = new Paragraph(LABEL, TITLE_FONT);
+		paragraphTitle.setAlignment(Element.TITLE);
+		addEmptyLine(paragraphTitle, 1);
 		document.add(paragraphTitle);
 
-		Paragraph paragraphOrderNumber = new Paragraph(ORDER_NUMBER + order.getId());
+		Paragraph paragraphOrderOpenDate = new Paragraph(OPEN_DATE + order.getOrderDate(), TABLE_FORT);
+		paragraphOrderOpenDate.setAlignment(Element.ALIGN_LEFT);
+		addEmptyLine(paragraphOrderOpenDate, 1);
+		document.add(paragraphOrderOpenDate);
+
+		Paragraph paragraphOrderCloseData = new Paragraph(CLOSE_DATE + order.getClosingDate(), TABLE_FORT);
+		paragraphOrderCloseData.setAlignment(Element.ALIGN_LEFT);
+		addEmptyLine(paragraphOrderCloseData, 2);
+		document.add(paragraphOrderCloseData);
+
+		Paragraph paragraphOrderNumber = new Paragraph(ORDER_NUMBER + order.getId(), TABLE_FORT);
 		paragraphOrderNumber.setAlignment(Element.ALIGN_CENTER);
 		addEmptyLine(paragraphOrderNumber, 1);
 		document.add(paragraphOrderNumber);
-		System.out.println(paragraphOrderNumber.toString());
-		
-		Paragraph orderProducts = new Paragraph("");
+
+		Paragraph orderProducts = new Paragraph(SPACE);
 		orderProducts.setAlignment(Element.ALIGN_CENTER);
 		createTable(orderProducts, listOrderContent);
 		document.add(orderProducts);
-		System.out.println(orderProducts.toString());
-		
-		Paragraph paragraphSumm = new Paragraph("Summ of order: " + order.getSum() + "uah");
+
+		Paragraph paragraphSumm = new Paragraph(ORDER_SUM + order.getSum() + GRN, TABLE_FORT);
 		paragraphSumm.setAlignment(Element.ALIGN_LEFT);
 		addEmptyLine(paragraphSumm, 1);
 		document.add(paragraphSumm);
-		System.out.println(paragraphSumm.toString());
-		
+
 		Paragraph paragraphCustomer = new Paragraph(
-				"Customer: " + order.getUserFirstName() + SPACE + order.getUserLastName());
+				CUSTOMER + order.getUserFirstName() + SPACE + order.getUserLastName(), TABLE_FORT);
 		paragraphCustomer.setAlignment(Element.ALIGN_LEFT);
 		document.add(paragraphCustomer);
-		System.out.println(paragraphCustomer.toString());
-		
-		Paragraph paragraphSeller = new Paragraph("Seller: " + SELLER);
+
+		Paragraph paragraphSeller = new Paragraph(SELLER + SELLER_LABEL, TABLE_FORT);
 		paragraphSeller.setAlignment(Element.ALIGN_LEFT);
 		document.add(paragraphSeller);
-		System.out.println(paragraphSeller.toString());
-		// Start a new page
+
 		document.newPage();
 	}
 
 	private static void createTable(Paragraph orderProducts, List<OrderContent> listOrderContent) {
 		PdfPTable table = new PdfPTable(COLUMN_COUNT);
-		PdfPCell cell = new PdfPCell(new Phrase("Name of product"));
+		PdfPCell cell = new PdfPCell(new Phrase(NAME, TABLE_FORT));
 		cell.setVerticalAlignment(Element.ALIGN_CENTER);
 		table.addCell(cell);
-		cell = new PdfPCell(new Phrase("Количество(шт)"));
+		cell = new PdfPCell(new Phrase(QUANTITY, TABLE_FORT));
 		cell.setVerticalAlignment(Element.ALIGN_CENTER);
 		table.addCell(cell);
-		cell = new PdfPCell(new Phrase("Цена(грн)"));
+		cell = new PdfPCell(new Phrase(PRICE_GRN, TABLE_FORT));
 		cell.setVerticalAlignment(Element.ALIGN_CENTER);
 		table.addCell(cell);
 		table.setHeaderRows(1);
 
 		for (OrderContent orderContetn : listOrderContent) {
-			table.addCell(new Phrase(String.valueOf(orderContetn.getProductName())));
-			table.addCell(new Phrase(String.valueOf(orderContetn.getProductCount())));
-			table.addCell(new Phrase(String.valueOf(orderContetn.getProductPrice())));
+			table.addCell(new Phrase(String.valueOf(orderContetn.getProductName()), TABLE_FORT));
+			table.addCell(new Phrase(String.valueOf(orderContetn.getProductCount()), TABLE_FORT));
+			table.addCell(new Phrase(String.valueOf(orderContetn.getProductPrice()), TABLE_FORT));
 		}
 		orderProducts.add(table);
 	}
 
 	private static void addEmptyLine(Paragraph paragraph, int number) {
 		for (int i = 0; i < number; i++) {
-			paragraph.add(new Paragraph(" "));
+			paragraph.add(new Paragraph(SPACE));
 		}
 	}
 
--- src/main/java/web/DownloadReport.java
+++ src/main/java/web/DownloadReport.java
@@ -20,8 +20,6 @@ import exception.DBException;
 import util.CreateFile;
 import util.Validator;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -68,6 +66,7 @@ public class DownloadReport extends HttpServlet {
 			LOG.error(Log.DB_EXCEPTION + e.getMessage());
 			response.sendError(500);
 			LOG.debug(Log.FINISH_WITH + 500);
+			return;
 		}
 
 		if (receipt == null) {
@@ -78,36 +77,20 @@ public class DownloadReport extends HttpServlet {
 
 		String fileName = receipt.getOrder().getClosingDate();
 
-		// response.setContentType("text/plain");
 		response.setContentType("application/pdf");
 		response.setCharacterEncoding("UTF-8");
-		// response.setHeader("Content-disposition", "attachment; filename=" + fileName
-		// + ".txt");
 		response.setHeader("Content-disposition", "attachment; filename=" + fileName + ".pdf");
 
-		// File reportFile = CreateFile.orderReport(receipt);
-		File reportFile;
-		try {
-			reportFile = CreateFile.createPdfFile(receipt);
-		} catch (Exception e) {
-			LOG.error(Log.EXCEPTION + e);
+		try (OutputStream out = response.getOutputStream()) {
+			CreateFile.writePdfFile(receipt, out);
+			out.flush();
+		} catch (DBException e) {
+			LOG.error(Log.DB_EXCEPTION + e.getMessage());
 			response.sendError(500);
 			LOG.debug(Log.FINISH_WITH + 500);
-
 			return;
 		}
 
-		try (OutputStream out = response.getOutputStream(); FileInputStream in = new FileInputStream(reportFile)) {
-
-			byte[] buffer = new byte[4096];
-			int length;
-
-			while ((length = in.read(buffer)) > 0) {
-				out.write(buffer, 0, length);
-			}
-
-			out.flush();
-		}
 		LOG.debug(Log.FINISH);
 	}
 
